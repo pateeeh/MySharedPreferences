51127abe9a9a7386c3973182bc527e4edb7b1b39
pateeh <fatihazzam33@gmail.com>
2025-03-01 16:44:39 +0700

    second commit

--- app/src/main/java/com/example/mysharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/mysharedpreferences/MainActivity.kt
@@ -1,20 +1,92 @@
 package com.example.mysharedpreferences
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mysharedpreferences.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mUserPreference: UserPreference
+    private var isPreferenceEmpty = false
+    private lateinit var userModel: UserModel
+    private lateinit var binding: ActivityMainBinding
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {result: ActivityResult ->
+        if (result.data != null && result.resultCode == FormUserPreferenceActivity.RESULT_CODE){
+            userModel = result.data?.getParcelableExtra<UserModel>(FormUserPreferenceActivity.EXTRA_RESULT) as UserModel
+            populateView(userModel)
+            checkForm(userModel)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "My User Preference"
+        mUserPreference = UserPreference(this)
+        showExistingPreference()
+
+        binding.btnSave.setOnClickListener (this)
+    }
+
+    private fun showExistingPreference() {
+        userModel = mUserPreference.getUser()
+        populateView(userModel)
+        checkForm(userModel)
+    }
+
+    private fun checkForm(userModel: UserModel) {
+        when{
+            userModel.name.toString().isEmpty() -> {
+                binding.btnSave.text = getString(R.string.change)
+                isPreferenceEmpty = false
+            }
+            else -> {
+                binding.btnSave.text = getString(R.string.save)
+                isPreferenceEmpty = true
+            }
+        }
+    }
+
+    private fun populateView(userModel: UserModel) {
+        binding.tvName.text = if (userModel.name.toString().isEmpty()) "Tidak ada" else userModel.name
+        binding.tvAge.text = if (userModel.age.toString().isEmpty()) "Tidak ada" else userModel.age.toString()
+        binding.tvEmail.text = if (userModel.email.toString().isEmpty()) "Tidak ada" else userModel.email
+        binding.tvPhone.text = if (userModel.phone.toString().isEmpty()) "Tidak ada" else userModel.phone
+        binding.tvIsLoveMu.text = if (userModel.isLove) "Ya" else "Tidak"
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_save) {
+            val intent = Intent(this, FormUserPreferenceActivity::class.java)
+            when {
+                isPreferenceEmpty -> {
+                    intent.putExtra(
+                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_ADD
+                    )
+                }
+                else -> {
+                    intent.putExtra(
+                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_EDIT
+                    )
+                    intent.putExtra("USER", userModel)
+                }
+            }
+            resultLauncher.launch(intent)
         }
     }
 }
